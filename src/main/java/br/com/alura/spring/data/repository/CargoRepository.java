e5174b27164fc102aa7c61b67505ced0c76544b9
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-05-18 04:52:34 -0300

    01.09 - Inserindo valores

--- src/main/java/br/com/alura/spring/data/repository/CargoRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/CargoRepository.java
@@ -3,8 +3,10 @@ package br.com.alura.spring.data.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.alura.spring.data.orm.Cargo;
+
 @Repository
-public interface CargoRepository extends CrudRepository {
+public interface CargoRepository extends CrudRepository<Cargo, Integer> {
 	
 
 }
