7f1c0dad22a60128087ca6b5d64e81ae5b7d3777
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-05-18 04:12:31 -0300

    01.06 Gerando o banco de dados

---
+++ src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -0,0 +1,34 @@
+package br.com.alura.spring.data.orm;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cargos")
+public class Cargo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String descricao;
+	
+	
+	public Cargo() {
+	}
+
+	public Cargo(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
+	
+}
